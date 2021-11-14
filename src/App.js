1b66f760a5ef6b2d5a9ec1388308467483cd3a44
Wojtek <wojciech.zbrozek@dreamstormstudios.com>
2021-11-14 14:01:05 +0100

    retrieved breed from url with appropriate formatting, added it as description

--- src/App.js
+++ src/App.js
@@ -4,28 +4,31 @@ import {useState, useEffect} from 'react'
 
 function App() {
   const [polaroidPhotos, setPolaroidPhotos] = useState([]);
-  // async function fetchPhoto() {
-  //   const response = await fetch('https://dog.ceo/api/breeds/image/random');
-  //   const photo = await response.json();
-  //   const photoSrc = await photo.message;
-  //   return photoSrc
-  // }
+  const formatBreedName = (url) => {
+    let unformattedBreed = url.substring(30, 30+url.substring(30).indexOf("/"));
+    let breed = unformattedBreed.split("-")
+    breed = (breed[1] ? breed[1] : '') + " " + breed[0];
+    breed = breed.trim();
+    breed = breed.charAt(0).toUpperCase() + breed.slice(1);
+    return breed;
+  }
   useEffect(()=>{    
     for(let i=0;i<3;i++){
       fetch('https://dog.ceo/api/breeds/image/random')
         .then(response => response.json())
-        .then(data => setPolaroidPhotos(polaroidPhotos => [...polaroidPhotos, data.message]))
+        .then(data => setPolaroidPhotos(polaroidPhotos => [...polaroidPhotos, {imgSrc: data.message, imgDesc: formatBreedName(data.message)
+        }]))
     }
   }, [])
   const moveLeft = () => {
     fetch('https://dog.ceo/api/breeds/image/random')
         .then(response => response.json())
-        .then(data => setPolaroidPhotos(polaroidPhotos => [ data.message, polaroidPhotos[0], polaroidPhotos[1]]))
+        .then(data => setPolaroidPhotos(polaroidPhotos => [ {imgSrc: data.message, imgDesc: "some description"}, polaroidPhotos[0], polaroidPhotos[1]]))
   }
   const moveRight = () => {
     fetch('https://dog.ceo/api/breeds/image/random')
         .then(response => response.json())
-        .then(data => setPolaroidPhotos(polaroidPhotos => [ polaroidPhotos[1], polaroidPhotos[2], data.message]))
+        .then(data => setPolaroidPhotos(polaroidPhotos => [ polaroidPhotos[1], polaroidPhotos[2], {imgSrc: data.message, imgDesc: "some description"}]))
   }
   return (
     <div className="App">
