6b21551bac157d6325ae7304006c86dc91023553
Wojtek <wojciech.zbrozek@dreamstormstudios.com>
2021-11-14 13:43:06 +0100

    fetching random photos of dogs with possibility to fech more using arrows

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import './App.css';
 import PhotoCarousel from './PhotoCarousel';
-
-const polaroidPhotos = [
-  {imgSrc: "/images/jpgtest.jpg", imgDesc: "Description 1"},
-  {imgSrc: "/images/jpgtest.jpg", imgDesc: "Description 2"},
-  {imgSrc: "/images/jpgtest.jpg", imgDesc: "Description 3"},
-]
+import {useState, useEffect} from 'react'
 
 function App() {
+  const [polaroidPhotos, setPolaroidPhotos] = useState([]);
+  // async function fetchPhoto() {
+  //   const response = await fetch('https://dog.ceo/api/breeds/image/random');
+  //   const photo = await response.json();
+  //   const photoSrc = await photo.message;
+  //   return photoSrc
+  // }
+  useEffect(()=>{    
+    for(let i=0;i<3;i++){
+      fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => response.json())
+        .then(data => setPolaroidPhotos(polaroidPhotos => [...polaroidPhotos, data.message]))
+    }
+  }, [])
+  const moveLeft = () => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => response.json())
+        .then(data => setPolaroidPhotos(polaroidPhotos => [ data.message, polaroidPhotos[0], polaroidPhotos[1]]))
+  }
+  const moveRight = () => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => response.json())
+        .then(data => setPolaroidPhotos(polaroidPhotos => [ polaroidPhotos[1], polaroidPhotos[2], data.message]))
+  }
   return (
     <div className="App">
-      <PhotoCarousel polaroidPhotos={polaroidPhotos}/>
+      <PhotoCarousel 
+        polaroidPhotos={polaroidPhotos} 
+        moveLeft={moveLeft} 
+        moveRight={moveRight}
+      />
     </div>
   );
 }
--- src/PhotoCarousel.js
+++ src/PhotoCarousel.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./photoCarousel.css"
 import PolaroidPhoto from './PolaroidPhoto';
 
 import {AiFillCaretLeft, AiFillCaretRight} from "react-icons/ai";
 
-const PhotoCarousel = ({polaroidPhotos}) => {
+const PhotoCarousel = ({polaroidPhotos, moveLeft, moveRight}) => {
+    useEffect(()=>{
+        console.log(polaroidPhotos)
+    },[])
     return (
         <div id="photoCarousel">
-            <div className="arrow"><AiFillCaretLeft /></div>
+            <div className="arrow" onClick={moveLeft}><AiFillCaretLeft /></div>
+            {/* <PolaroidPhoto imgSrc={polaroidPhotos[0]} imgDesc="some desc" /> */}
             {polaroidPhotos.map(polaroidPhoto => {
-                return <PolaroidPhoto imgSrc={polaroidPhoto.imgSrc} imgDesc={polaroidPhoto.imgDesc} />
+                return <PolaroidPhoto imgSrc={polaroidPhoto} imgDesc="some desc" />
             })}
-            <div className="arrow"><AiFillCaretRight /></div>
+            <div className="arrow" onClick={moveRight}><AiFillCaretRight /></div>
         </div>
     )
 }
