d3b1ae1b2705798a0aa73e0b81954a1ccd566313
Wojciech Zbrożek <wzbrozek@polsl.pl>
2021-11-15 10:56:49 +0100

    image loading beforehand, basic responsive design

--- src/PhotoCarousel.js
+++ src/PhotoCarousel.js
@@ -7,19 +7,36 @@ const PhotoCarousel = ({polaroidPhotos}) => {
 
     const [bound, setBound] = useState(0)
 
+    const slide = (direction) => {
+        direction==="left"?
+            polaroidPhotos.push(polaroidPhotos.shift()):
+            polaroidPhotos.unshift(polaroidPhotos.pop());
+    }
+
     return (
         <div id="photoCarousel">
-            <div className="arrow" onClick={()=>{
+
+            <div className="arrowWrapper left" onClick={()=>{
                 bound===0?setBound(9):setBound((bound-1)%polaroidPhotos.length)
-                console.log(bound)
-            }}><AiFillCaretLeft /></div>
-                {polaroidPhotos[bound%polaroidPhotos.length]}
+                slide("left");
+            }}>
+                <div className="arrow"><AiFillCaretLeft /></div>
+            </div>
+
+            <div id="photoContainer">
+                {/* {polaroidPhotos[(bound)%polaroidPhotos.length]}
                 {polaroidPhotos[(bound+1)%polaroidPhotos.length]}
-                {polaroidPhotos[(bound+2)%polaroidPhotos.length]}
-            <div className="arrow" onClick={()=>{
+                {polaroidPhotos[(bound+2)%polaroidPhotos.length]} */}
+                {polaroidPhotos.map(photo=>photo)}
+            </div>
+
+            <div className="arrowWrapper right" onClick={()=>{
                 setBound((bound+1)%polaroidPhotos.length)
-                console.log(bound)
-            }}><AiFillCaretRight /></div>
+                slide("right");
+            }}>
+                <div className="arrow"><AiFillCaretRight /></div>
+            </div>
+
         </div>
     )
 }
